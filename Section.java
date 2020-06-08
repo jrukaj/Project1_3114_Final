489b03c67ac0b9800266fe4b9c21dcdd99aa5261
Dilan-Prasad <dilanp06@gmail.com>
2020-06-08 16:47:46 -0400

    Update Section.java

--- Section.java
+++ Section.java
@@ -31,35 +31,7 @@ public class Section<T> {
         return tree.toString();
     } 
     
-    public ArrayList<ArrayList<String>> getSection(ArrayList<ArrayList<String>> list) {
-        ArrayList<ArrayList<String>> secList = new ArrayList<ArrayList<String>>();
-        for (ArrayList<String> strList: list) {
-            if (strList.get(0).equals("section") && strList.get(1).equals("1")) {
-                flag1 = true;
-                flag2 = false;
-                flag3 = false;
-            }
-            
-            else if (strList.get(0).equals("section") && strList.get(1).equals("2")) {
-                flag1 = false;
-                flag2 = true;
-                flag3 = false;
-            }
-            
-            else if (strList.get(0).equals("section") && strList.get(1).equals("3")) {
-                flag1 = false;
-                flag2 = false;
-                flag3 = true; 
-            }
-            secList.add(strList);
-            // use boolean flags to determine what section we are in
-            // ie, when flag1 is true, all following commands are to be put in section 1
-            
-            // not sure 
-        }
-        
-        return secList;
-    }
+
     
     public void setSecNum(int val) {
         this.secNum = val;
